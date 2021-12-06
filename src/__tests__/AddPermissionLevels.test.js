ab2c2830495f5294854a8fb1535fa7c024d1d40c
Ganesh Potharam <ganesh.potharam@toyota.com>
2021-12-06 16:47:02 +0530

    Testing

--- src/__tests__/AddPermissionLevels.test.js
+++ src/__tests__/AddPermissionLevels.test.js
@@ -1,51 +1,60 @@
 import React from "react";
 import Enzyme from "enzyme";
-import Adapter from "@wojtekmaj/enzyme-adapter-react-17";
 import { render } from "@testing-library/react";
+import Adapter from "@wojtekmaj/enzyme-adapter-react-17";
 import AddPermissionLevels from "../components/AddPermissionLevels/js/AddPermissionLevels";
 
 Enzyme.configure({ adapter: new Adapter() });
+const datetype = "datetime-local";
+const descPlace = "Add the Description here..";
+const category = "Catergory Name*";
+const startdate = "Start date & Time*";
+const enddate = "End Date & Time*";
+const sampledata = {
+	brand: "Toyota",
+	startDate: "11/23/2021 02:32:00 AM",
+	endDate: "01/04/2022 02:32:00 AM",
+	description: "Vehicle Connected Service description added",
+	comments: [
+		{
+			user: "abc@xyz.com",
+			comment: "Vehicle Connected Services",
+			time: "11/23/2021 02:02:51 PM",
+		},
+	],
+	status: "Live",
+	rank: 1,
+	level: 2,
+	createdDate: "11/30/2021 02:10:45 AM",
+	modifiedDate: "11/30/2021 02:10:45 AM",
+	createdBy: "user",
+	modifiedBy: "",
+	id: "CATEGORY#TEST_COMMUNICATIONS#VEHICLE_CONNECTED_SERVICE",
+	subCategoryName: "Vehicle Connected Service",
+};
 
-const sampledata = [
-  {
-    brand: "Toyota",
-    startDate: "11/23/2021 02:32:00 AM",
-    endDate: "01/04/2022 02:32:00 AM",
-    description: "Vehicle Connected Service description added",
-    comments: [
-      {
-        user: "abc@xyz.com",
-        comment: "Vehicle Connected Services",
-        time: "11/23/2021 02:02:51 PM",
-      },
-    ],
-    status: "Live",
-    rank: 1,
-    level: 2,
-    createdDate: "11/30/2021 02:10:45 AM",
-    modifiedDate: "11/30/2021 02:10:45 AM",
-    createdBy: "user",
-    modifiedBy: "",
-    id: "CATEGORY#TEST_COMMUNICATIONS#VEHICLE_CONNECTED_SERVICE",
-    subCategoryName: "Vehicle Connected Service",
-  },
-];
-
-it("Add Sub Permission Levels renders properly", () => {
-  const wrapper = render(
-    <AddPermissionLevels
-      show={true}
-      onClose={() => {
-        console.log("Closed");
-      }}
-      category={sampledata}
-      optionType={"Add"}
-      brand={"Toyota"}
-      notify={() => {
-        console.log("Notify");
-      }}
-      level={2}
-    />
-  );
-  expect(wrapper.getByPlaceholderText("Catergory Name*").value).toBe("");
+it("Edit Sub Permission Levels renders properly", () => {
+	const wrapper = render(
+		<AddPermissionLevels
+			show={true}
+			onClose={() => {
+				console.log("Closed");
+			}}
+			category={sampledata}
+			optionType={"Edit"}
+			brand={"Toyota"}
+			notify={() => {
+				console.log("Notify");
+			}}
+			level={2}
+		/>
+	);
+	expect(wrapper.getByPlaceholderText(category).value).toBe(
+		sampledata.subCategoryName
+	);
+	expect(wrapper.getByPlaceholderText(descPlace).value).toBe(
+		sampledata.description
+	);
+	expect(wrapper.getByPlaceholderText(startdate).type).toBe(datetype);
+	expect(wrapper.getByPlaceholderText(enddate).type).toBe(datetype);
 });
